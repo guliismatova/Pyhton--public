d2d1d398493393cd933fad631c598a9370e3a952
Guli_10 <137134526+guliismatova@users.noreply.github.com>
2023-06-22 19:08:08 +0500

    Add files via upload

---
+++ python 4masala yec 2-yo'l.py
@@ -0,0 +1,15 @@
+a,b,c = map(int,input().split())
+if a<0:
+    print(a)
+if b<0:
+    print(b)
+if c<0:
+    print(c)
+print(max(a,b,c))
+print(min(a,b,c))
+if a>0:
+    print(a)
+if b>0:
+    print(b)
+if c>0:
+    print(c)
